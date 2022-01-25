85fa441da919ac8d2b53885d27a11e9fc43ec963
Tony Bai <bigwhite.cn@aliyun.com>
2022-01-25 21:54:41 +0800

    go-first-course: add source for 37

---
+++ column/timegeek/go-first-course/37/tcp-server-demo1/cmd/client/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"sync"
+	"time"
+
+	"github.com/bigwhite/tcp-server-demo1/frame"
+	"github.com/bigwhite/tcp-server-demo1/packet"
+	"github.com/lucasepe/codename"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	var num int = 5
+
+	wg.Add(5)
+
+	for i := 0; i < num; i++ {
+		go func(i int) {
+			defer wg.Done()
+			startClient(i)
+		}(i + 1)
+	}
+	wg.Wait()
+}
+
+func startClient(i int) {
+	quit := make(chan struct{})
+	done := make(chan struct{})
+	conn, err := net.Dial("tcp", ":8888")
+	if err != nil {
+		fmt.Println("dial error:", err)
+		return
+	}
+	defer conn.Close()
+	fmt.Printf("[client %d]: dial ok", i)
+
+	// 生成payload
+	rng, err := codename.DefaultRNG()
+	if err != nil {
+		panic(err)
+	}
+
+	frameCodec := frame.NewMyFrameCodec()
+	var counter int
+
+	go func() {
+		// handle ack
+		for {
+			select {
+			case <-quit:
+				done <- struct{}{}
+				return
+			default:
+			}
+
+			conn.SetReadDeadline(time.Now().Add(time.Second * 1))
+			ackFramePayLoad, err := frameCodec.Decode(conn)
+			if err != nil {
+				if e, ok := err.(net.Error); ok {
+					if e.Timeout() {
+						continue
+					}
+				}
+				panic(err)
+			}
+
+			p, err := packet.Decode(ackFramePayLoad)
+			submitAck, ok := p.(*packet.SubmitAck)
+			if !ok {
+				panic("not submitack")
+			}
+			fmt.Printf("[client %d]: the result of submit ack[%s] is %d\n", i, submitAck.ID, submitAck.Result)
+		}
+	}()
+
+	for {
+		// send submit
+		counter++
+		id := fmt.Sprintf("%08d", counter) // 8 byte string
+		payload := codename.Generate(rng, 4)
+		s := &packet.Submit{
+			ID:      id,
+			Payload: []byte(payload),
+		}
+
+		framePayload, err := packet.Encode(s)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("[client %d]: send submit id = %s, payload=%s, frame length = %d\n",
+			i, s.ID, s.Payload, len(framePayload)+4)
+
+		err = frameCodec.Encode(conn, framePayload)
+		if err != nil {
+			panic(err)
+		}
+
+		time.Sleep(1 * time.Second)
+		if counter >= 10 {
+			quit <- struct{}{}
+			<-done
+			fmt.Printf("[client %d]: exit ok\n", i)
+			return
+		}
+	}
+}
---
+++ column/timegeek/go-first-course/37/tcp-server-demo1/go.mod
@@ -0,0 +1,5 @@
+module github.com/bigwhite/tcp-server-demo1
+
+go 1.17
+
+require github.com/lucasepe/codename v0.2.0 // indirect
